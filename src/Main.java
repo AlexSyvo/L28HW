aefc63ab7a6da49206cc35ebbfa81a00e3b20e39
AlexSyvo <killmore@poczta.fm>
2025-02-06 18:48:27 +0100

    factorial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(factorial(5));
     }
+    public static int factorial(int a){
+       if (a==1){
+        return a;
+    }
+
+        return a*factorial(a-1);
+    }
+
 }
